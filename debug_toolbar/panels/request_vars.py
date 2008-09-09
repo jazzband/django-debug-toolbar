ad5b511eaa86738e6ec58a91c3601369ad60eb82
Rob Hudson <rob@cogit8.org>
2008-09-09 12:51:28 -0700

    Merging in some changes from David Cramer for the HTTP vars panel but renaming it to request vars since we've added session and cookie data (sessions have nothing to do with HTTP technically).

---
+++ debug_toolbar/panels/request_vars.py
@@ -0,0 +1,23 @@
+from django.template.loader import render_to_string
+from debug_toolbar.panels import DebugPanel
+
+class RequestVarsDebugPanel(DebugPanel):
+    """
+    A panel to display request variables (POST/GET, session, cookies).
+    """
+    name = 'RequestVars'
+
+    def title(self):
+        return 'Request Vars'
+
+    def url(self):
+        return ''
+
+    def content(self):
+        context = {
+            'get': [(k, self.request.GET.getlist(k)) for k in self.request.GET.iterkeys()],
+            'post': [(k, self.request.POST.getlist(k)) for k in self.request.POST.iterkeys()],
+            'session': [(k, self.request.session.get(k)) for k in self.request.session.iterkeys()],
+            'cookies': [(k, self.request.COOKIES.get(k)) for k in self.request.COOKIES.iterkeys()],
+        }
+        return render_to_string('debug_toolbar/panels/request_vars.html', context)
