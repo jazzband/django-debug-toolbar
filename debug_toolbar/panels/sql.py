49a177a41f790a494a933963fd7b2f5216f98a85
Rob Hudson <rob@cogit8.org>
2008-10-06 12:54:02 -0700

    Updating SQL output a bit now that I've found more queries to test against.

--- debug_toolbar/panels/sql.py
+++ debug_toolbar/panels/sql.py
@@ -71,9 +71,9 @@ def reformat_sql(sql):
     sql = sql.replace('`,`', '`, `')
     sql = sql.replace('SELECT ', 'SELECT\n\t')
     sql = sql.replace('` FROM ', '`\nFROM\n\t')
-    sql = sql.replace('` WHERE ', '`\nWHERE\n\t')
-    sql = sql.replace('` INNER JOIN ', '`\nINNER JOIN\n\t')
-    sql = sql.replace('` OUTER JOIN ', '`\nOUTER JOIN\n\t')
+    sql = sql.replace(' WHERE ', '\nWHERE\n\t')
+    sql = sql.replace(' INNER JOIN ', '\nINNER JOIN\n\t')
+    sql = sql.replace(' OUTER JOIN ', '\nOUTER JOIN\n\t')
     sql = sql.replace(' ORDER BY ', '\nORDER BY\n\t')
     # Use Pygments to highlight SQL if it's available
     try:
