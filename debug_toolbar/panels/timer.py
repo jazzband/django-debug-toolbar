d58f119fc17bd15edee5f3ac783703a8aec534b8
Martin Maney <maney@two14.net>
2008-11-08 11:33:19 -0600

    Added fine-grained CPU usage to timer

--- debug_toolbar/panels/timer.py
+++ debug_toolbar/panels/timer.py
@@ -1,4 +1,4 @@
-import time
+import time, resource
 from debug_toolbar.panels import DebugPanel
 
 class TimerDebugPanel(DebugPanel):
@@ -9,12 +9,16 @@ class TimerDebugPanel(DebugPanel):
 
     def process_request(self, request):
         self._start_time = time.time()
+        self._start_rusage = resource.getrusage(resource.RUSAGE_SELF)
 
     def process_response(self, request, response):
         self.total_time = (time.time() - self._start_time) * 1000
+        self._end_rusage = resource.getrusage(resource.RUSAGE_SELF)
 
     def title(self):
-        return 'Time: %0.2fms' % (self.total_time)
+        utime = self._end_rusage.ru_utime - self._start_rusage.ru_utime
+        stime = self._end_rusage.ru_stime - self._start_rusage.ru_stime
+        return 'Time: %0.2fms, %0.2fms CPU' % (self.total_time, (utime + stime) * 1000.0)
 
     def url(self):
         return ''
