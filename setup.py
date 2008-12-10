ec08e18be48929e83b1c14ae13d439e5d6bdf2fc
Rob Hudson <rob@cogit8.org>
2008-12-10 11:30:02 -0800

    Some tweaks and changes to the setup.py file.

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
 
 version = '0.1'
 
-setup(name='debug_toolbar',
-      version=version,
-      description="A configurable set of panels that display various debug information about the current request/response.",
-      long_description=open("README.rst").read(),
-      # Get more strings from http://www.python.org/pypi?:action=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Development Status :: 4 - Beta",
-        "Environment :: Plugins",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Framework :: Django",
-        ],
-      keywords='',
-      author='Rob Hudson',
-      author_email='rob@cogit8.com',
-      url='http://rob.cogit8.org/blog/2008/Sep/19/introducing-django-debug-toolbar/',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=[],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-      ],
-      entry_points="""
-      """,
-      )
+setup(
+    name='debug_toolbar',
+    version=version,
+    description='A configurable set of panels that display various debug information about the current request/response.',
+    long_description=open('README.rst').read(),
+    # Get more strings from http://www.python.org/pypi?:action=list_classifiers
+    author='Rob Hudson',
+    author_email='rob@cogit8.org',
+    url='http://rob.cogit8.org/blog/2008/Sep/19/introducing-django-debug-toolbar/',
+    download_url='http://github.com/robhudson/django-debug-toolbar/tree/master',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
