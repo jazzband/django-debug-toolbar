0e6ef21e1f94a5064778466313b84d7f572ac164
Rob Hudson <rob@cogit8.org>
2008-12-12 15:19:33 -0800

    Fine tuning setup.py for Pypi release.

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
-import os
 from setuptools import setup, find_packages
 
-version = '0.1'
-
 setup(
-    name='debug_toolbar',
-    version=version,
+    name='django-debug-toolbar',
+    version=__import__('debug_toolbar').__version__,
     description='A configurable set of panels that display various debug information about the current request/response.',
     long_description=open('README.rst').read(),
     # Get more strings from http://www.python.org/pypi?:action=list_classifiers
     author='Rob Hudson',
     author_email='rob@cogit8.org',
     url='http://rob.cogit8.org/blog/2008/Sep/19/introducing-django-debug-toolbar/',
-    download_url='http://github.com/robhudson/django-debug-toolbar/tree/master',
+    #download_url='http://github.com/robhudson/django-debug-toolbar/tree/master',
     license='BSD',
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
-    zip_safe=False,
+    zip_safe=False, # because we're including media that Django needs
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
