4f680980f9987063c663e46ab190c7aeba552ac7
Loic Bistuer <loic.bistuer@sixmedia.com>
2009-03-14 00:15:46 +0700

    fixed bug when the string </body> matches more than once in the html
    Signed-off-by: Rob Hudson <rob@cogit8.org>

--- debug_toolbar/middleware.py
+++ debug_toolbar/middleware.py
@@ -19,7 +19,7 @@ def replace_insensitive(string, target, replacement):
     Code borrowed from: http://forums.devshed.com/python-programming-11/case-insensitive-string-replace-490921.html
     """
     no_case = string.lower()
-    index = no_case.find(target.lower())
+    index = no_case.rfind(target.lower())
     if index >= 0:
         return string[:index] + replacement + string[index + len(target):]
     else: # no results so return the original string
