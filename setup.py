4f43c9b1004bac18e5323e52c4fb110fd4f2b2ba
Rob Hudson <rob@cogit8.org>
2009-09-12 20:44:30 -0700

    Updated URL to the GitHub page (soon to be finished)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Get more strings from http://www.python.org/pypi?:action=list_classifiers
     author='Rob Hudson',
     author_email='rob@cogit8.org',
-    url='http://rob.cogit8.org/blog/2008/Sep/19/introducing-django-debug-toolbar/',
+    url='http://robhudson.github.com/django-debug-toolbar/',
     download_url='http://github.com/robhudson/django-debug-toolbar/downloads',
     license='BSD',
     packages=find_packages(exclude=['ez_setup']),
