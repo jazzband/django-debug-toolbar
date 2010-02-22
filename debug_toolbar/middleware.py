508d83c4111c5884ca7a520b1ccfe7b6c68f2aaf
Jannis Leidel <jannis@leidel.info>
2010-02-22 15:12:40 +0100

    Also check for HTTP_X_FORWARDED_FOR in case a proxy like Nginx is used
    Signed-off-by: Rob Hudson <rob@cogit8.org>

--- debug_toolbar/middleware.py
+++ debug_toolbar/middleware.py
@@ -52,8 +52,13 @@ class DebugToolbarMiddleware(object):
                 self.tag = u'</' + tag + u'>'
 
     def _show_toolbar(self, request):
-        if not request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS \
-                or request.is_ajax() or not settings.DEBUG:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR', None)
+        if x_forwarded_for:
+            remote_addr = x_forwarded_for.split(',')[0].strip()
+        else:
+            remote_addr = request.META.get('REMOTE_ADDR', None)
+        if not remote_addr in settings.INTERNAL_IPS \
+                or request.is_ajax() or not setting.DEBUG:
             return False
         return True
 
