ee1811238e91ae0ad33413b0d40d2f8482101951
Rob Hudson <rob@cogit8.org>
2010-04-07 07:51:50 -0700

    Revert "Start of splitting off data collection classes and panel classes."
    This reverts commit edf74beb6e26ed34779dff17240d2fb4c436d451.

--- debug_toolbar/panels/timer.py
+++ debug_toolbar/panels/timer.py
@@ -6,36 +6,42 @@ import time
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 from debug_toolbar.panels import DebugPanel
-from debug_toolbar.debug.timer import DebugTimer
 
 class TimerDebugPanel(DebugPanel):
     """
     Panel that displays the time a response took in milliseconds.
     """
     name = 'Timer'
-
-    def __init__(self, context={}):
-        super(TimerDebugPanel, self).__init__(context)
-        self.timer = DebugTimer()
-        self.has_content = self.timer.has_resource
+    try: # if resource module not available, don't show content panel
+        resource
+    except NameError:
+        has_content = False
+        has_resource = False
+    else:
+        has_content = True
+        has_resource = True
 
     def process_request(self, request):
-        self.timer.start(request)
+        self._start_time = time.time()
+        if self.has_resource:
+            self._start_rusage = resource.getrusage(resource.RUSAGE_SELF)
 
     def process_response(self, request, response):
-        self.timer.stop(request, response)
+        self.total_time = (time.time() - self._start_time) * 1000
+        if self.has_resource:
+            self._end_rusage = resource.getrusage(resource.RUSAGE_SELF)
 
     def nav_title(self):
         return _('Time')
 
     def nav_subtitle(self):
         # TODO l10n
-        if self.timer.has_resource:
-            utime = self.timer._end_rusage.ru_utime - self.timer._start_rusage.ru_utime
-            stime = self.timer._end_rusage.ru_stime - self.timer._start_rusage.ru_stime
-            return 'CPU: %0.2fms (%0.2fms)' % ((utime + stime) * 1000.0, self.timer.total_time)
+        if self.has_resource:
+            utime = self._end_rusage.ru_utime - self._start_rusage.ru_utime
+            stime = self._end_rusage.ru_stime - self._start_rusage.ru_stime
+            return 'CPU: %0.2fms (%0.2fms)' % ((utime + stime) * 1000.0, self.total_time)
         else:
-            return 'TOTAL: %0.2fms' % (self.timer.total_time)
+            return 'TOTAL: %0.2fms' % (self.total_time)
 
     def title(self):
         return _('Resource Usage')
@@ -43,14 +49,17 @@ class TimerDebugPanel(DebugPanel):
     def url(self):
         return ''
 
+    def _elapsed_ru(self, name):
+        return getattr(self._end_rusage, name) - getattr(self._start_rusage, name)
+
     def content(self):
 
-        utime = 1000 * self.timer.elapsed_ru('ru_utime')
-        stime = 1000 * self.timer.elapsed_ru('ru_stime')
-        vcsw = self.timer.elapsed_ru('ru_nvcsw')
-        ivcsw = self.timer.elapsed_ru('ru_nivcsw')
-        minflt = self.timer.elapsed_ru('ru_minflt')
-        majflt = self.timer.elapsed_ru('ru_majflt')
+        utime = 1000 * self._elapsed_ru('ru_utime')
+        stime = 1000 * self._elapsed_ru('ru_stime')
+        vcsw = self._elapsed_ru('ru_nvcsw')
+        ivcsw = self._elapsed_ru('ru_nivcsw')
+        minflt = self._elapsed_ru('ru_minflt')
+        majflt = self._elapsed_ru('ru_majflt')
 
 # these are documented as not meaningful under Linux.  If you're running BSD
 # feel free to enable them, and add any others that I hadn't gotten to before
@@ -69,7 +78,7 @@ class TimerDebugPanel(DebugPanel):
             (_('User CPU time'), '%0.3f msec' % utime),
             (_('System CPU time'), '%0.3f msec' % stime),
             (_('Total CPU time'), '%0.3f msec' % (utime + stime)),
-            (_('Elapsed time'), '%0.3f msec' % self.timer.total_time),
+            (_('Elapsed time'), '%0.3f msec' % self.total_time),
             (_('Context switches'), '%d voluntary, %d involuntary' % (vcsw, ivcsw)),
 #            ('Memory use', '%d max RSS, %d shared, %d unshared' % (rss, srss, urss + usrss)),
 #            ('Page faults', '%d no i/o, %d requiring i/o' % (minflt, majflt)),
