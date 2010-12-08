278d70affb9d720e25e54b4f04913150fa0b0c2c
Matthew J Morrison <matt.morrison@theimtgroup.com>
2010-12-08 08:47:20 -0600

    changed version to dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-toolbar',
-    version=__import__('debug_toolbar').__version__,
+    version='dev',
     description='A configurable set of panels that display various debug information about the current request/response.',
     long_description=open('README.rst').read(),
     # Get more strings from http://www.python.org/pypi?:action=list_classifiers
