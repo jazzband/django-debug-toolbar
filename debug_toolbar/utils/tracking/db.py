679c3214d6777171fa0bca9f8d1c5267c2dbbfaa
David Cramer <dcramer@gmail.com>
2011-05-16 14:09:31 -0700

    alias should be derived from cursor's db, not the cursor itself

--- debug_toolbar/utils/tracking/db.py
+++ debug_toolbar/utils/tracking/db.py
@@ -57,7 +57,7 @@ class CursorWrapper(object):
                 pass
             del cur_frame
 
-            alias = getattr(self, 'alias', 'default')
+            alias = getattr(self.db, 'alias', 'default')
             conn = connections[alias].connection
             # HACK: avoid imports
             if conn:
