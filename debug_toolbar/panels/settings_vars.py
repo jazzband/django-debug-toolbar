fa8e9aab09cd47370749e1a03f2e90e6a7a93faf
bkonkle <brandon@konkle.us>
2011-09-12 12:11:38 -0500

    Updated the settings_vars panel

--- debug_toolbar/panels/settings_vars.py
+++ debug_toolbar/panels/settings_vars.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.template.loader import render_to_string
 from django.views.debug import get_safe_settings
 from django.utils.translation import ugettext_lazy as _
+from debug_toolbar.middleware import DebugToolbarMiddleware
 from debug_toolbar.panels import DebugPanel
 
 
@@ -11,19 +12,24 @@ class SettingsVarsDebugPanel(DebugPanel):
     """
     name = 'SettingsVars'
     has_content = True
-
+    
     def nav_title(self):
         return _('Settings')
-
+    
     def title(self):
         return _('Settings from <code>%s</code>') % settings.SETTINGS_MODULE
-
+    
     def url(self):
         return ''
-
+    
+    def process_response(self, request, response):
+        self.stats = {
+            'settings': get_safe_settings(),
+        }
+        toolbar = DebugToolbarMiddleware.get_current()
+        toolbar.stats['settings_vars'] = self.stats
+    
     def content(self):
         context = self.context.copy()
-        context.update({
-            'settings': get_safe_settings(),
-        })
+        context.update(self.stats)
         return render_to_string('debug_toolbar/panels/settings_vars.html', context)
