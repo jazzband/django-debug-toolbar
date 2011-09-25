e6a1fcb0a21161ecb4f3393a2cfd3cc591c83543
Danilo Bargen <gezuru@gmail.com>
2011-09-25 22:12:17 +0200

    Changed datetime.today to datetime.now

--- debug_toolbar/management/commands/debugsqlshell.py
+++ debug_toolbar/management/commands/debugsqlshell.py
@@ -9,12 +9,12 @@ from debug_toolbar.utils import sqlparse
 
 class PrintQueryWrapper(util.CursorDebugWrapper):
     def execute(self, sql, params=()):
-        starttime = datetime.today()
+        starttime = datetime.now()
         try:
             return self.cursor.execute(sql, params)
         finally:
             raw_sql = self.db.ops.last_executed_query(self.cursor, sql, params)
-            execution_time = datetime.today() - starttime
+            execution_time = datetime.now() - starttime
             print sqlparse.format(raw_sql, reindent=True)
             print
             print 'Execution time: %fs' % execution_time.total_seconds()
