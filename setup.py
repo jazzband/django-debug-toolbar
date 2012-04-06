36bb1787c363c5dc4feba251f98aea20a8e23a6f
Jannis Leidel <jannis@leidel.info>
2012-04-06 13:09:01 +0200

    Added a Django 1.4 test environment to Travis config.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license='BSD',
     packages=find_packages(exclude=('tests', 'example')),
     tests_require=[
-        'django>=1.1,<1.4',
+        'django>=1.1,<1.5',
         'dingus',
     ],
     test_suite='runtests.runtests',
