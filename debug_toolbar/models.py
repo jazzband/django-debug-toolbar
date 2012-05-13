7f7ea810c1a8ccf2d4211c55d2d635ace66141e7
Jannis Leidel <jannis@leidel.info>
2012-05-13 12:14:04 +0200

    Fixed the toolbar loading mechanism to only happen when the middleware has been added to the MIDDLEWARE_CLASSES setting.

--- debug_toolbar/models.py
+++ debug_toolbar/models.py
@@ -1,3 +1,30 @@
-from debug_toolbar.toolbar.loader import load_panel_classes
+from django.conf import settings
+from django.utils.importlib import import_module
 
-load_panel_classes()
+from debug_toolbar.toolbar.loader import load_panel_classes
+from debug_toolbar.middleware import DebugToolbarMiddleware
+
+loaded = False
+
+
+def is_toolbar(cls):
+    return (issubclass(cls, DebugToolbarMiddleware) or
+            DebugToolbarMiddleware in getattr(cls, '__bases__', ()))
+
+
+def iter_toolbar_middlewares():
+    global loaded
+    for middleware_path in settings.MIDDLEWARE_CLASSES:
+        try:
+            mod_path, cls_name = middleware_path.rsplit('.', 1)
+            mod = import_module(mod_path)
+            middleware_cls = getattr(mod, cls_name)
+        except (AttributeError, ImportError, ValueError):
+            continue
+        if is_toolbar(middleware_cls) and not loaded:
+            # we have a hit!
+            loaded = True
+            yield middleware_cls
+
+for middleware_cls in iter_toolbar_middlewares():
+    load_panel_classes()
