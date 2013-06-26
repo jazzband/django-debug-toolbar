ba6f6fee512e4b8403005daa48141bb6f6618a02
Darie Petrov <darie@cambriansoft.org>
2013-06-26 15:57:23 -0400

    updated deprecated urls.default import to work with django 1.6

--- example/urls.py
+++ example/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.contrib import admin
 from django.views.generic import TemplateView
 
