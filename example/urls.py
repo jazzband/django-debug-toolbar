a87f34be05ed4f9ba41eecacc7968eeb287a8c87
Alasdair Nicol <alasdair@memset.com>
2013-08-22 15:52:37 +0100

    Avoided importing `django.conf.urls.defaults` in example project
    Use same approach as 39088e7cab

--- example/urls.py
+++ example/urls.py
@@ -1,5 +1,8 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError: # django < 1.4
+    from django.conf.urls.defaults import patterns, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
