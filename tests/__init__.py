b8d3c1c9043c782f15e9cea9c5d4f50ca5bae46a
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-11-09 19:38:47 +0100

    Invalidate cached list of panels during tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 # Refresh the debug toolbar's configuration when overriding settings.
 
 from debug_toolbar.utils.settings import CONFIG, CONFIG_DEFAULTS
+from debug_toolbar.toolbar.loader import load_panel_classes, panel_classes
+
 from django.dispatch import receiver
 from django.test.signals import setting_changed
 
@@ -10,3 +12,11 @@ def update_toolbar_config(**kwargs):
     if kwargs['setting'] == 'DEBUG_TOOLBAR_CONFIG':
         CONFIG.update(CONFIG_DEFAULTS)
         CONFIG.update(kwargs['value'] or {})
+
+
+@receiver(setting_changed)
+def update_toolbar_panels(**kwargs):
+    if kwargs['setting'] == 'DEBUG_TOOLBAR_PANELS':
+        global panel_classes
+        panel_classes = []
+        load_panel_classes()
