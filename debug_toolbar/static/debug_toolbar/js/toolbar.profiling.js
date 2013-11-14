4cce59d4c244a755181de5579dabc0d0e26335ee
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-11-14 22:23:51 +0100

    Move some panel-specific javascript in specific files.

---
+++ debug_toolbar/static/debug_toolbar/js/toolbar.profiling.js
@@ -0,0 +1,27 @@
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else {
+        factory(jQuery);
+    }
+}(function ($) {
+    function getSubcalls(row) {
+        var id = row.attr('id');
+        return $('.djDebugProfileRow[id^="'+id+'_"]');
+    }
+    function getDirectSubcalls(row) {
+        var subcalls = getSubcalls(row);
+        var depth = parseInt(row.attr('depth'), 10) + 1;
+        return subcalls.filter('[depth='+depth+']');
+    }
+    $('.djDebugProfileRow .djDebugProfileToggle').on('click', function(){
+        var row = $(this).closest('.djDebugProfileRow');
+        var subcalls = getSubcalls(row);
+        if (subcalls.css('display') == 'none') {
+            getDirectSubcalls(row).show();
+        } else {
+            subcalls.hide();
+        }
+    });
+
+}));
