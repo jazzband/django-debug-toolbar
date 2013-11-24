b9c5c503428323927c8d602bedc214cb3415ad35
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-11-24 14:24:49 +0100

    Shorten developers list.

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Django Debug Toolbar'
-copyright = u'2013, Rob Hudson, Jannis Leidel, David Cramer, Aymeric Augustin and contributors'
+copyright = u'2013, Django Debug Toolbar developers and contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -200,7 +200,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'DjangoDebugToolbar.tex', u'Django Debug Toolbar Documentation',
-   u'Rob Hudson, Jannis Leidel, David Cramer, Aymeric Augustin and contributors', 'manual'),
+   u'Django Debug Toolbar developers and contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -230,7 +230,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'djangodebugtoolbar', u'Django Debug Toolbar Documentation',
-     [u'Rob Hudson, Jannis Leidel, David Cramer, Aymeric Augustin and contributors'], 1)
+     [u'Django Debug Toolbar developers and contributors'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -244,7 +244,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'DjangoDebugToolbar', u'Django Debug Toolbar Documentation',
-   u'Rob Hudson, Jannis Leidel, David Cramer, Aymeric Augustin and contributors', 'DjangoDebugToolbar', 'One line description of project.',
+   u'Django Debug Toolbar developers and contributors', 'DjangoDebugToolbar', 'One line description of project.',
    'Miscellaneous'),
 ]
 
