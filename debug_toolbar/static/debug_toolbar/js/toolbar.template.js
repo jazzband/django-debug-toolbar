67c855ac1548b0f00ced8944c76453dd118ae09d
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-11-29 23:46:12 +0100

    Fix JS regression in the template panel.
    It was introduced during last week-end's large renaming.

--- debug_toolbar/static/debug_toolbar/js/toolbar.template.js
+++ debug_toolbar/static/debug_toolbar/js/toolbar.template.js
@@ -8,7 +8,7 @@
     var uarr = String.fromCharCode(0x25b6),
         darr = String.fromCharCode(0x25bc);
 
-    $('#djDebugTemplatePanel a.djTemplateShowContext').on('click', function() {
+    $('a.djTemplateShowContext').on('click', function() {
         var arrow = $(this).children('.toggleArrow');
         arrow.html(arrow.html() == uarr ? darr : uarr);
         $(this).parent().next().toggle();
