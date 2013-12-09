e4fafb92377221201f9089aa972ac6c5504b45dd
Jannis Leidel <jannis@leidel.info>
2013-12-09 18:39:25 +0100

    More test relaxing.

--- tests/panels/test_staticfiles.py
+++ tests/panels/test_staticfiles.py
@@ -23,7 +23,7 @@ class StaticFilesPanelTestCase(BaseTestCase):
         self.assertIn('django.contrib.staticfiles.finders.'
                       'FileSystemFinder (1 file)', self.panel.content)
         self.assertEqual(self.panel.num_used, 0)
-        self.assertEqual(self.panel.num_found, 88)
+        self.assertNotEqual(self.panel.num_found, 0)
         self.assertEqual(self.panel.get_staticfiles_apps(),
                          ['django.contrib.admin', 'debug_toolbar'])
         self.assertEqual(self.panel.get_staticfiles_dirs(),
