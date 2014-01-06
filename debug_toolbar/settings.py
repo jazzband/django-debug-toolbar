2613b0d19f02b3b356383c1e6268ffbdb4027897
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-01-06 22:52:52 +0100

    Default DEBUG_TOOLBAR_PANELS didn't match the docs.
    Fix #519.

--- debug_toolbar/settings.py
+++ debug_toolbar/settings.py
@@ -75,13 +75,12 @@ PANELS_DEFAULTS = [
     'debug_toolbar.panels.headers.HeadersPanel',
     'debug_toolbar.panels.request.RequestPanel',
     'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
     'debug_toolbar.panels.cache.CachePanel',
     'debug_toolbar.panels.signals.SignalsPanel',
     'debug_toolbar.panels.logging.LoggingPanel',
     'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
 ]
 
 try:
