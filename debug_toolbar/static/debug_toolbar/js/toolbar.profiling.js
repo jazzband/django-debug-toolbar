4b27834b9d8d500c44eddc411dda13b1bfb65bef
Denis Stebunov <denis.stebunov@ivelum.com>
2014-04-18 15:23:32 +0400

    more generic solution with support for non-RequireJS environments

--- debug_toolbar/static/debug_toolbar/js/toolbar.profiling.js
+++ debug_toolbar/static/debug_toolbar/js/toolbar.profiling.js
@@ -1,7 +1,12 @@
 (function (factory) {
-    if (typeof define === 'function' && define.amd && typeof require === 'function') {
-        // RequireJS, use jQuery from its config
-        require(['jquery'], factory);
+    if (typeof define === 'function' && define.amd) {
+        if (typeof require === 'function') {
+          // RequireJS, use jQuery from its config
+          require(['jquery'], factory);
+        } else {
+          // AMD, but not RequireJS. Register as anonymous module.
+          define(['jquery'], factory);
+        }
     } else {
         factory(jQuery);
     }
