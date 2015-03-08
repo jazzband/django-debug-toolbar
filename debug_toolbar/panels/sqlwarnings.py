cb995d68b10c12fafd7241f2c6bbc1dd004df3db
KS Chan <mrkschan@gmail.com>
2015-03-08 22:33:10 +0800

    SQL Warnings Panel now sends EXPLAIN to ask for execution plan

--- debug_toolbar/panels/sqlwarnings.py
+++ debug_toolbar/panels/sqlwarnings.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import json
 
 from django.db import connections
 from django.utils.translation import ugettext_lazy as _
@@ -6,7 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from debug_toolbar.panels import Panel
 from debug_toolbar.panels.sql.tracking import ThreadLocalState
-from debug_toolbar.panels.sql.utils import reformat_sql
 
 
 state = ThreadLocalState()
@@ -54,12 +54,48 @@ class SQLWarningsPanel(Panel):
             unwrap_cursor(conn)
 
     def process_response(self, request, response):
-        # FIXME: Call EXPLAIN and detect any non-scalable SQL execution plan.
-        sqls = [(db, reformat_sql(query['sql']))
-                for db, queries in self._databases.items()
-                for query in queries]
-        self.record_stats({'sqls': sqls})
+        self._cursors = {}
+
+        sqls = ((alias, query['vendor'], query['raw_sql'], query['params'])
+                for alias, queries in self._databases.items()
+                for query in queries)
+        plans = (self.explain(alias, vendor, sql, params)
+                 for alias, vendor, sql, params in sqls)
+        evals = (self.evaluate(alias, vendor, sql, plan)
+                 for alias, vendor, sql, plan in plans)
+        warnings = ((alias, vendor, sql, warn)
+                    for alias, vendor, sql, warn in evals if warn)
+
+        self.record_stats({'warnings': list(warnings)})
+
+        for alias, cursor in self._cursors.items():
+            cursor.close()
 
     def record(self, alias, **kwargs):
         # 'sql.tracking.ThreadLocalState.Wrapper' would call this function.
         self._databases[alias].append(kwargs)
+
+    def explain(self, alias, vendor, sql, params):
+        if alias not in self._cursors:
+            conn = connections[alias]
+            factory = getattr(conn, '_djdt_sqlwarnings_cursor', conn.cursor)
+            self._cursors[alias] = factory()
+
+        cursor = self._cursors[alias]
+        params = json.loads(params)
+
+        if vendor == 'sqlite':
+            cursor.execute('EXPLAIN QUERY PLAN %s' % sql, params)
+        elif vendor == 'postgresql':
+            cursor.execute('EXPLAIN ANALYZE %s' % sql, params)
+        else:
+            cursor.execute('EXPLAIN %s' % sql, params)
+
+        # FIXME: Serialize execution plan
+        plan = cursor.fetchall()
+
+        return alias, vendor, sql, plan
+
+    def evaluate(self, alias, vendor, sql, plan):
+        warning = plan  # FIXME: generate warning based on SQL execution plan
+        return alias, vendor, sql, warning
