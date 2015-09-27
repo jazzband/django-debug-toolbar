1586da91f0a47beaa9ec74c044e760681950cd0c
calvinpy <dani@aplazame.com>
2015-09-27 23:08:50 +0200

    remove STATUS_CODE_TEXT

--- debug_toolbar/panels/redirects.py
+++ debug_toolbar/panels/redirects.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.core.handlers.wsgi import STATUS_CODE_TEXT
 from django.shortcuts import render_to_response
 from django.utils.translation import ugettext_lazy as _
+from django.utils.six.moves.http_client import responses
 
 from debug_toolbar.panels import Panel
 
@@ -23,8 +23,8 @@ class RedirectsPanel(Panel):
                 try:        # Django >= 1.6
                     reason_phrase = response.reason_phrase
                 except AttributeError:
-                    reason_phrase = STATUS_CODE_TEXT.get(response.status_code,
-                                                         'UNKNOWN STATUS CODE')
+                    reason_phrase = responses.get(response.status_code,
+                                                  'UNKNOWN STATUS CODE')
                 status_line = '%s %s' % (response.status_code, reason_phrase)
                 cookies = response.cookies
                 context = {'redirect_to': redirect_to, 'status_line': status_line}
