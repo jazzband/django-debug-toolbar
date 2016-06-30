2f5ee13a922264768eec1c86dbee31f9cef97409
Jon Dufresne <jon.dufresne@gmail.com>
2016-06-29 17:31:05 -0700

    Handle deprecation of app_name argument to urls.include in Django 1.9
    Warning appears as:
    
    RemovedInDjango20Warning: Passing a 3-tuple to
    django.conf.urls.include() is deprecated. Pass a 2-tuple containing the
    list of patterns and app_name, and provide the namespace argument to
    include() instead.
    
    Fixes #764

--- debug_toolbar/__init__.py
+++ debug_toolbar/__init__.py
@@ -12,8 +12,10 @@ except Exception:
 
 # Code that discovers files or modules in INSTALLED_APPS imports this module.
 # Reference URLpatterns with a string to avoid the risk of circular imports.
-
-urls = 'debug_toolbar.toolbar', 'djdt', 'djdt'
-
+import django
+if django.VERSION < (1, 9):
+    urls = 'debug_toolbar.toolbar', 'djdt', 'djdt'
+else:
+    urls = 'debug_toolbar.toolbar', 'djdt'
 
 default_app_config = 'debug_toolbar.apps.DebugToolbarConfig'
