a87e6fc4c797edc8a7b99ec99d961f2e4f53d244
Kim SJ <kim@spencejones.com>
2017-02-24 10:42:35 +0000

    Bullet-proof the test for window.define in jquery_pre.js and ..._post.js

--- debug_toolbar/static/debug_toolbar/js/jquery_post.js
+++ debug_toolbar/static/debug_toolbar/js/jquery_post.js
@@ -1,4 +1,4 @@
 var djdt = {jQuery: jQuery.noConflict(true)};
-if (window.define) {
+if (typeof window.define is "function" && window.define.amd) {
     window.define.amd = _djdt_define_amd_backup;
 }
--- debug_toolbar/static/debug_toolbar/js/jquery_pre.js
+++ debug_toolbar/static/debug_toolbar/js/jquery_pre.js
@@ -1,5 +1,5 @@
 var _djdt_define_amd_backup;
-if (window.define) {
+if (typeof window.define is "function" && window.define.amd) {
     _djdt_define_amd_backup = window.define.amd;
     window.define.amd = undefined;
 }
