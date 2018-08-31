970a6d4728eae24641ae26fd4f01fb552eb6e94d
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-08-31 14:17:49 +0200

    rm .djDebugToggle
    This is duplicated functionality. You can already toggle the entries
    using .djToggleSwitch

--- debug_toolbar/static/debug_toolbar/js/toolbar.sql.js
+++ debug_toolbar/static/debug_toolbar/js/toolbar.sql.js
@@ -1,9 +1,4 @@
 (function ($) {
-    $('#djDebug a.djDebugToggle').on('click', function(event) {
-        event.preventDefault();
-        $(this).parent().find('.djDebugCollapsed').toggle();
-        $(this).parent().find('.djDebugUncollapsed').toggle();
-    });
     djdt.applyStyle('background-color');
     djdt.applyStyle('left');
     djdt.applyStyle('width');
