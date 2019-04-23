769e90548f8e61873d1eed439c5b8d8fb973a3fe
Alex Tomkins <tomkins@darkzone.net>
2019-04-23 14:12:24 +0100

    Add test to show repr template rendering

---
+++ tests/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class TemplateReprForm(forms.Form):
+    user = forms.ModelChoiceField(queryset=User.objects.all())
+
+    def __repr__(self):
+        return str(self)
