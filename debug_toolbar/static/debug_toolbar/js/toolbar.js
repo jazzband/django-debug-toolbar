jazzband/django-debug-toolbar

commit ea8def535bd4c297fcc2ad36f5cebf9b0764e2a6

Simplify URL building using the URL class (#1355)
https://developer.mozilla.org/en-US/docs/Web/API/URL

debug_toolbar/static/debug_toolbar/js/toolbar.js
@@ -100,11 +100,9 @@ const djdt = {
                         ),
                         store_id = djDebug.dataset.storeId;
                     if (store_id && inner.children.length === 0) {
-                        let url = djDebug.dataset.renderPanelUrl;
-                        const url_params = new URLSearchParams();
-                        url_params.append("store_id", store_id);
-                        url_params.append("panel_id", this.className);
-                        url += "?" + url_params.toString();
+                        let url = new URL(djDebug.dataset.renderPanelUrl, window.location);
+                        url.searchParams.append("store_id", store_id);
+                        url.searchParams.append("panel_id", this.className);
                         ajax(url).then(function (data) {
                             inner.previousElementSibling.remove(); // Remove AJAX loader
                             inner.innerHTML = data.content;

commit 454d098849697be025320fad7690ef43fba607fb

Fix eslint/prettier errors
Errors introduced in ea8def535bd4c297fcc2ad36f5cebf9b0764e2a6.

(Sorry 😳)

debug_toolbar/static/debug_toolbar/js/toolbar.js
@@ -100,7 +100,10 @@ const djdt = {
                         ),
                         store_id = djDebug.dataset.storeId;
                     if (store_id && inner.children.length === 0) {
-                        let url = new URL(djDebug.dataset.renderPanelUrl, window.location);
+                        const url = new URL(
+                            djDebug.dataset.renderPanelUrl,
+                            window.location
+                        );
                         url.searchParams.append("store_id", store_id);
                         url.searchParams.append("panel_id", this.className);
                         ajax(url).then(function (data) {