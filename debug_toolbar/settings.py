e1864e9e622cefdece77bb422328f876ce5ae2d1
Tim Schilling <schillingt@better-simple.com>
2021-01-22 10:12:20 -0600

    Move PRETTIFY_SQL in the settings to be alphabetical.

--- debug_toolbar/settings.py
+++ debug_toolbar/settings.py
@@ -37,11 +37,11 @@ CONFIG_DEFAULTS = {
         "django.utils.deprecation",
         "django.utils.functional",
     ),
+    "PRETTIFY_SQL": True,
     "PROFILER_MAX_DEPTH": 10,
     "SHOW_TEMPLATE_CONTEXT": True,
     "SKIP_TEMPLATE_PREFIXES": ("django/forms/widgets/", "admin/widgets/"),
     "SQL_WARNING_THRESHOLD": 500,  # milliseconds
-    "PRETTIFY_SQL": True,
 }
 
 
