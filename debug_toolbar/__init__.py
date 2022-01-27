ff0b974378ecf3e2d6f7f5a404d3c62d0e633912
Matthias Kestenholz <mk@feinheit.ch>
2022-01-27 14:58:36 +0100

    Fix #1581: Duplicate the 'djdt' app_name to avoid importing the toolbar early

--- debug_toolbar/__init__.py
+++ debug_toolbar/__init__.py
@@ -1,12 +1,8 @@
-from debug_toolbar.urls import app_name
-
 __all__ = ["VERSION"]
-
 
 # Do not use pkg_resources to find the version but set it here directly!
 # see issue #1446
 VERSION = "3.2.4"
 
 # Code that discovers files or modules in INSTALLED_APPS imports this module.
-
-urls = "debug_toolbar.urls", app_name
+urls = "debug_toolbar.urls", "djdt"  # See debug_toolbar/urls.py
--- debug_toolbar/urls.py
+++ debug_toolbar/urls.py
@@ -1,4 +1,4 @@
 from debug_toolbar.toolbar import DebugToolbar
 
-app_name = "djdt"
+app_name = "djdt"  # See debug_toolbar/__init__.py
 urlpatterns = DebugToolbar.get_urls()
