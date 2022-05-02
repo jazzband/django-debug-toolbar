c39b84bde3c0f502b67d92528e61febbd6deb58a
Tim Schilling <schilling711@gmail.com>
2022-05-02 11:33:05 -0500

    The path may not always be a true path for stacktraces. (#1613)
    Occassionally we will get a stacktrace that's an importlib instance string
    representation. While we may be able to put the python path or something else
    it's likely easier (and more logical) to simply pass that onto the user.
    
    I was unable to reproduce the issue in our tests, so I've mocked the case
    in test_importlib_path_issue_1612.
    
    Fixes #1612

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from debug_toolbar.utils import get_name_from_obj
+from debug_toolbar.utils import get_name_from_obj, render_stacktrace
 
 
 class GetNameFromObjTestCase(unittest.TestCase):
@@ -21,3 +21,29 @@ class GetNameFromObjTestCase(unittest.TestCase):
 
         res = get_name_from_obj(A)
         self.assertEqual(res, "tests.test_utils.A")
+
+
+class RenderStacktraceTestCase(unittest.TestCase):
+    def test_importlib_path_issue_1612(self):
+        trace = [
+            ("/server/app.py", 1, "foo", ["code line 1", "code line 2"], {"foo": "bar"})
+        ]
+        result = render_stacktrace(trace)
+        self.assertIn('<span class="djdt-path">/server/</span>', result)
+        self.assertIn('<span class="djdt-file">app.py</span> in', result)
+
+        trace = [
+            (
+                "<frozen importlib._bootstrap>",
+                1,
+                "foo",
+                ["code line 1", "code line 2"],
+                {"foo": "bar"},
+            )
+        ]
+        result = render_stacktrace(trace)
+        self.assertIn('<span class="djdt-path"></span>', result)
+        self.assertIn(
+            '<span class="djdt-file">&lt;frozen importlib._bootstrap&gt;</span> in',
+            result,
+        )
