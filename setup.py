1cf55d9ec0b2de3f0296ad608a923d780aea83c1
Ofek Lev <ofekmeister@gmail.com>
2022-10-24 10:27:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
+
+import sys
+
+from setuptools import setup
+
+sys.stderr.write(
+    """\
+===============================
+Unsupported installation method
+===============================
+This project no longer supports installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
+
 # The code below will never execute, however is required to
 # display the "Used by" section on the GitHub repository.
 #
 # See: https://github.com/github/feedback/discussions/6456
-
-from setuptools import setup
 
 setup(name="django-debug-toolbar")
