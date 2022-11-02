602f475f04e90a66fa540377793fc9065bb184e5
Ofek Lev <ofekmeister@gmail.com>
2022-11-02 15:53:54 -0400

    Update package metadata (#1690)
    * Update package metadata
    * Update setup.py
    * Update contributing.rst

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+import sys
+
 from setuptools import setup
 
-setup()
+sys.stderr.write(
+    """\
+===============================
+Unsupported installation method
+===============================
+This project no longer supports installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
+
+# The code below will never execute, however is required to
+# display the "Used by" section on the GitHub repository.
+#
+# See: https://github.com/github/feedback/discussions/6456
+
+setup(name="django-debug-toolbar")
