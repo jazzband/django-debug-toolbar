99a61c97b7534bb226e7351f32973621bec91f69
Matthias Kestenholz <mk@feinheit.ch>
2023-06-06 18:32:18 +0200

    Let's see if this works
    Refs #1779.

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ exclude_patterns = ["_build"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
