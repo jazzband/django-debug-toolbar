97fcda7270ab959aba4a6fc2fab0b2a0ca062972
tschilling <schillingt@better-simple.com>
2023-06-17 10:15:18 -0500

    Switch to Store.request_ids and remove serialization force_str.
    If the serialization logic begins throwing exceptions we can consider
    subclassing the encoder class and using force_str on the object itself.

--- debug_toolbar/store.py
+++ debug_toolbar/store.py
@@ -1,24 +1,19 @@
+import contextlib
 import json
 from collections import defaultdict, deque
 from typing import Any, Dict, Iterable
 
 from django.core.serializers.json import DjangoJSONEncoder
-from django.utils.encoding import force_str
 from django.utils.module_loading import import_string
 
 from debug_toolbar import settings as dt_settings
 
 
-class DebugToolbarJSONEncoder(DjangoJSONEncoder):
-    def default(self, o: Any) -> Any:
-        try:
-            return super().default(o)
-        except TypeError:
-            return force_str(o)
-
-
 def serialize(data: Any) -> str:
-    return json.dumps(data, cls=DebugToolbarJSONEncoder)
+    # If this starts throwing an exceptions, consider
+    # Subclassing DjangoJSONEncoder and using force_str to
+    # make it JSON serializable.
+    return json.dumps(data, cls=DjangoJSONEncoder)
 
 
 def deserialize(data: str) -> Any:
@@ -29,8 +24,8 @@ class BaseStore:
     _config = dt_settings.get_config().copy()
 
     @classmethod
-    def ids(cls) -> Iterable:
-        """The stored ids"""
+    def request_ids(cls) -> Iterable:
+        """The stored request ids"""
         raise NotImplementedError
 
     @classmethod
@@ -68,43 +63,41 @@ class MemoryStore(BaseStore):
     # ids is the collection of storage ids that have been used.
     # Use a dequeue to support O(1) appends and pops
     # from either direction.
-    _ids: deque = deque()
+    _request_ids: deque = deque()
     _request_store: Dict[str, Dict] = defaultdict(dict)
 
     @classmethod
-    def ids(cls) -> Iterable:
-        """The stored ids"""
-        return cls._ids
+    def request_ids(cls) -> Iterable:
+        """The stored request ids"""
+        return cls._request_ids
 
     @classmethod
     def exists(cls, request_id: str) -> bool:
         """Does the given request_id exist in the request store"""
-        return request_id in cls._ids
+        return request_id in cls._request_ids
 
     @classmethod
     def set(cls, request_id: str):
         """Set a request_id in the request store"""
-        if request_id not in cls._ids:
-            cls._ids.append(request_id)
-        for _ in range(len(cls._ids) - cls._config["RESULTS_CACHE_SIZE"]):
-            removed_id = cls._ids.popleft()
+        if request_id not in cls._request_ids:
+            cls._request_ids.append(request_id)
+        for _ in range(len(cls._request_ids) - cls._config["RESULTS_CACHE_SIZE"]):
+            removed_id = cls._request_ids.popleft()
             cls._request_store.pop(removed_id, None)
 
     @classmethod
     def clear(cls):
         """Remove all requests from the request store"""
-        cls._ids.clear()
+        cls._request_ids.clear()
         cls._request_store.clear()
 
     @classmethod
     def delete(cls, request_id: str):
         """Delete the stored request for the given request_id"""
         cls._request_store.pop(request_id, None)
-        try:
-            cls._ids.remove(request_id)
-        except ValueError:
-            # The request_id doesn't exist in the collection of ids.
-            pass
+        # Suppress when request_id doesn't exist in the collection of ids.
+        with contextlib.suppress(ValueError):
+            cls._request_ids.remove(request_id)
 
     @classmethod
     def save_panel(cls, request_id: str, panel_id: str, data: Any = None):
