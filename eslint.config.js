585b16388f317fe82508424b9a053255de8218cc
Elijah Okello <elijahokello90@gmail.com>
2024-02-13 16:27:24 +0300

    fix: added dependencies and added support for globals in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,17 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
 module.exports = [
+    js.configs.recommended,
     {
+        files: ["**/*.js"],
         languageOptions:{
-            ecmaVersion: 6,
+            ecmaVersion: 2022,
             sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            }
         }
     },
     {
