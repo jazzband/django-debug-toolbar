1f3a6dc2d6968dd3204c0a66532ce89362c6e4e6
Tim Schilling <schillingt@better-simple.com>
2024-02-19 07:25:25 -0600

    Switch to the render function so we can include context.

--- example/views.py
+++ example/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 
 
 def increment(request):
@@ -8,3 +9,7 @@ def increment(request):
         value = 1
     request.session["value"] = value
     return JsonResponse({"value": value})
+
+
+def jinja2_view(request):
+    return render(request, "index.jinja", {"foo": "bar"}, using="jinja2")
