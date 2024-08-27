7bd53143d400d26673a7e9910bc04610f523435e
Aman Pandey <aman2001mi@gmail.com>
2024-08-27 21:38:04 +0530

    add asgi and aenable_instrumentation in wordlist

--- debug_toolbar/panels/__init__.py
+++ debug_toolbar/panels/__init__.py
@@ -155,7 +155,7 @@ class Panel:
         Unless the toolbar or this panel is disabled, this method will be
         called early in ``DebugToolbarMiddleware``. It should be idempotent.
 
-        Add ``aenable_instrumentation`` method to the Panel class to support async
+        Add aenable_instrumentation method to the Panel class to support async
         instrumentation logic.
         """
 
